90a36090666b3313a83810eaa5c3f0c3178cd501
Daniel Moskal <daniel19967@gmail.com>
2020-02-09 16:36:55 +0100

    171 - Manage interval observable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'user/:id', component: UserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { interval, Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit, OnDestroy {
+
+  intervalSub: Subscription;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.intervalSub = interval(1000).subscribe(count => {
+      console.log(count);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.intervalSub.unsubscribe();
+  }
+
+}
+
