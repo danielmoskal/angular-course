e898d2cea060198a3d8ddc39d7290a8af76c1d01
Daniel Moskal <daniel19967@gmail.com>
2020-02-14 20:59:31 +0100

    194 - Using ngModel with two-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f') form: NgForm;
   defaultQuestion = 'teacher';
+  reply: string;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
